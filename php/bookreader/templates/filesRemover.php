ed221dee3d5c775a323d0324dc7725b45ed60599
suresh07 <soori_rozee@yahoo.com>
2015-06-29 15:44:50 +0530

    Commit No. 38 - bug fix in fileremover

--- php/bookreader/templates/filesRemover.php
+++ php/bookreader/templates/filesRemover.php
@@ -3,10 +3,13 @@
 	exec($jpgBulletin);
 	$jpgBlb = 'find ../../../Volumes/blb/jpg/1/ -mmin +10 -type f -name "*.jpg" -exec rm {} \;';
 	exec($jpgBlb);
+	$jpgSpecial = 'find ../../../Volumes/special/jpg/1/ -mmin +10 -type f -name "*.jpg" -exec rm {} \;';
+	exec($jpgSpecial);
 	$tifBulletin = 'find ../../../Volumes/bulletin/tif/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
 	exec($tifBulletin);
 	$tifBlb = 'find ../../../Volumes/blb/tif/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
 	exec($tifBlb);
-	$pdf = 'find ../../../ReadWrite/ -mmin +5 -type f -name "*.pdf" -exec rm {} \;';
-	exec($pdf);
+	$tifSpecial = 'find ../../../Volumes/special/tif/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
+	exec($tifSpecial);
+	
 ?>
